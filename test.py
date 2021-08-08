6bf4134e8aedb69725d80e03ae06caffa0b3f5fc
growvv <2092876368@qq.com>
2021-08-09 00:13:03 +0800

    test action

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import datetime
 import time
-from reserve import send_sms
+from reserve import send_email, send_sms
 import os
 
 
@@ -11,6 +11,9 @@ SMS_SID = os.environ['SMS_SID']
 SMS_TOKEN = os.environ['SMS_TOKEN']
 SMS_FROM_NUMBER = os.environ['SMS_FROM_NUMBER']
 SMS_TO_NUMBER = os.environ['SMS_TO_NUMBER']
-print(SMS_FROM_NUMBER+"12213")
+print(SMS_FROM_NUMBER[:-2])
 print(SMS_SID)
-send_sms(SMS_TO_NUMBER, "你好，测试")
+# send_sms(SMS_TO_NUMBER, "你好，测试")
+
+EMAIL_TO = os.environ['EMAIL_TO']
+send_email(EMAIL_TO, '体育馆预约', "你好")
